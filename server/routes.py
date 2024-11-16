ee217f0213fbe6527c73cf88d16c6a33a29251f6
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2024-11-16 13:27:22 -0500

    Tentative routes.py

---
+++ server/routes.py
@@ -0,0 +1,80 @@
+from time import sleep
+from flask import Flask, Response, request
+from typing import TYPE_CHECKING, Any, Dict, List, Tuple
+from queue import Queue as SingleQueue
+from multiprocessing import Queue as MultiQueue
+from database import get_conversation, delta_response
+
+if(TYPE_CHECKING):
+    from main import Command
+
+command_queue: MultiQueue[Tuple[Command, List[str]]]
+def set_command_queue(new_queue: MultiQueue[Tuple[Command, List[str]]]):
+    global command_queue
+    command_queue = new_queue
+
+global_streaming_queue: MultiQueue[Tuple[str, str | None]]
+def set_global_streaming_queue(new_queue: MultiQueue[Tuple[str, str | None]]):
+    global global_streaming_queue
+    global_streaming_queue = new_queue
+
+local_streaming_queue_map: Dict[str, SingleQueue[str | None]] = dict()
+def start_streaming_processor():
+    while(True):
+        (id, delta) = global_streaming_queue.get(True, None)
+        local_streaming_queue = local_streaming_queue_map.get(id)
+        if(local_streaming_queue != None):
+            local_streaming_queue.put(delta)
+        else:
+            print(f"Error: Local streaming queue does not exist for id {id}")
+
+def get_body() -> Tuple[None | Dict[str, Any], int]:
+    data = request.get_json(silent=True)
+    if(data == None):
+        return (None, 415)
+    if(not isinstance(data, dict)):
+        return (None, 400)
+    return (data, 200)
+
+
+app = Flask(__name__)
+
+@app.route("/chat", methods=[ "POST" ])
+def ask():
+    data, status_code = get_body()
+    if(data == None):
+        return Response(None, status_code)
+    id = data.get("id")
+    message = data.get("message")
+    if(not isinstance(id, str) or not isinstance(message, str)):
+        return Response(None, 400)
+    conversation = get_conversation(id)
+    if(conversation == None):
+        return Response(None, 400)
+    
+    conversation.messages.append(message) # User message
+    conversation.messages.append("") # Assitant message
+    local_streaming_queue: SingleQueue[str | None] = SingleQueue()
+    local_streaming_queue_map[id] = local_streaming_queue
+    command_queue.put( ("completion", conversation.messages) )
+
+    def eventStream():
+        done = False
+        while(not done):
+            delta = ""
+            while(local_streaming_queue.not_empty):
+                try:
+                    fragment = local_streaming_queue.get(False)
+                    if(fragment != None):
+                        delta += fragment
+                    else:
+                        done = True
+                except:
+                    break
+            if(delta != ""):
+                delta_response(id, delta)
+                yield f"event: delta\ndata: {delta}\n\n"
+            sleep(0.5)
+        del local_streaming_queue_map[id]
+
+    return Response(eventStream(), mimetype="text/event-stream")

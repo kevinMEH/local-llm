4f0ad120284732da70cb6b4e5de48eebf9d7b175
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2024-11-17 12:01:25 -0500

    Single model philosophy

--- server/controller.py
+++ server/controller.py
@@ -1,31 +1,24 @@
 # Accepts commands and dispatches to models
-from multiprocessing import Process, Queue as MultiQueue
+from multiprocessing import Queue as MultiQueue
 from threading import Thread
-from typing import Tuple
-from models.text_generation_pipeline import get_streamer, generate
 from Command import Command
+from models.text_generation_pipeline import load_model, unload_model, generate_and_cleanup
 
-# As of right now, children are responsible for loading model
-def generate_to_queue(global_streaming_queue: "MultiQueue[Tuple[str, str | None]]", command: Command):
-    id = command["id"]
-    model_id = command["model_id"]
-    messages = command["messages"]
-    streamer = get_streamer(model_id)
-    thread = Thread(target=generate, args=(streamer, model_id, messages))
-    thread.start()
-    for delta in streamer:
-        global_streaming_queue.put((id, delta))
-    global_streaming_queue.put((id, None))
-    thread.join()
-
-def start_commands_processor(
+def process_commands(
     command_queue: "MultiQueue[Command]",
-    global_streaming_queue: "MultiQueue[Tuple[str, str | None]]"
+    streaming_queue: "MultiQueue[str | None]"
 ):
     while True:
         command = command_queue.get(True, None)
         instruction = command["instruction"]
         if(instruction == "completion"):
-            generation = Process(target=generate_to_queue, args=(global_streaming_queue, command))
-            generation.start()
-            generation.join() # NOTE: As of right now, only one generation at a time
+            model_id = command["model_id"]
+            messages = command["messages"]
+            streamer = load_model(model_id)
+            thread = Thread(target=generate_and_cleanup, args=tuple([messages]))
+            thread.start()
+            for delta in streamer:
+                streaming_queue.put(delta)
+            streaming_queue.put(None)
+            thread.join()
+            unload_model()

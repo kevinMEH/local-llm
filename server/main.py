59010b1cbb2bc39800495d298af93460ff5e9e0c
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2024-11-17 05:20:03 -0500

    main.py

--- server/main.py
+++ server/main.py
@@ -1,24 +1,34 @@
 from multiprocessing import Process, Queue as MultiQueue
 from threading import Thread
-from typing import List, Literal, Tuple
-from routes import app, set_global_streaming_queue, set_command_queue, start_streaming_processor
-
-type Command = Literal["completion"]
+from typing import Tuple
+from routes import app, start_streaming_processor
+from routes import set_command_queue as routes_set_command_queue
+from routes import set_global_streaming_queue as routes_set_global_streaming_queue
+from controller import start_commands_processor
+from Command import Command
 
 def start_server(
-    command_queue: MultiQueue[Tuple[Command, List[str]]],
-    global_streaming_queue: MultiQueue[Tuple[str, str | None]]
+    command_queue: "MultiQueue[Command]",
+    global_streaming_queue: "MultiQueue[Tuple[str, str | None]]"
 ):
-    set_command_queue(command_queue)
-    set_global_streaming_queue(global_streaming_queue)
+    routes_set_command_queue(command_queue)
+    routes_set_global_streaming_queue(global_streaming_queue)
     thread = Thread(target=start_streaming_processor)
     thread.start()
     app.run(port=2778, debug=False)
+    thread.join()
 
 if __name__ == "__main__":
     command_queue = MultiQueue()
     global_streaming_queue = MultiQueue()
     routes = Process(target=start_server, args=(command_queue, global_streaming_queue))
-    routes.start()
-    routes.join()
-    print("after join")
+    controller = Process(target=start_commands_processor, args=(command_queue, global_streaming_queue))
+    try:
+        routes.start()
+        controller.start()
+    except KeyboardInterrupt:
+        routes.terminate()
+        controller.terminate()
+    finally:
+        routes.join()
+        controller.join()

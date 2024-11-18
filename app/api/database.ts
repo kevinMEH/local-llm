ebe761f64b0d8b2cc2607f0d312819b06ae249dc
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2024-11-19 01:20:29 +0800

    Removed test data from database.ts

--- app/api/database.ts
+++ app/api/database.ts
@@ -25,28 +25,6 @@ declare global {
 if(process.env.NODE_ENV !== "production") {
     if(!global.database) {
         global.database = new Map<string, Conversation>();
-        let id;
-        id = randomId(16);
-        global.database.set(id, {
-            id,
-            title: "First",
-            model_id: "meta-llama/Llama-3.2-3B-Instruct",
-            messages: []
-        });
-        id = randomId(16);
-        global.database.set(id, {
-            id,
-            title: "Second",
-            model_id: "meta-llama/Llama-3.2-3B-Instruct",
-            messages: []
-        });
-        id = randomId(16);
-        global.database.set(id, {
-            id,
-            title: "Third",
-            model_id: "meta-llama/Llama-3.2-3B-Instruct",
-            messages: []
-        });
     }
     database = global.database;
 } else {

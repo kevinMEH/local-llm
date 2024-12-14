b5d44774579f2a69df52c1ac6501960f7982ee6c
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2024-12-14 15:27:47 +0800

    Misc completions fix

--- app/api/completions.ts
+++ app/api/completions.ts
@@ -6,7 +6,7 @@ export async function completions(model: string, messages: string[]) {
     const stream = createStreamableValue("");
     
     (async () => {
-        const response = await fetch("http://localhost:2778/chat/completions", {
+        const response = await fetch("http://127.0.0.1:2778/chat/completions", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
@@ -19,13 +19,11 @@ export async function completions(model: string, messages: string[]) {
             stream.error("Error: Completion failed for some unknown reason.");
             return;
         }
-        messages.push("");
         const decoder = new TextDecoder();
         for await(const data of (response.body as unknown as AsyncIterable<Uint8Array>)) {
             const parts = decoder.decode(data).trim().split("\n");
             if(parts[0] == "event: delta") {
                 const value = JSON.parse(parts[1].substring(6)).v as string;
-                messages.push(messages.pop() + value);
                 stream.update(value);
             }
         }

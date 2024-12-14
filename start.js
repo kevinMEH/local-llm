361aee45e38d50f3ed6fcfaf6a98c8fb52b0bafc
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2024-12-14 13:23:25 +0800

    start.js start script

---
+++ start.js
@@ -0,0 +1,42 @@
+import { spawn as __spawn } from "child_process";
+import { existsSync } from "fs";
+
+/**
+ * @param  {Parameters<__spawn>} args 
+ */
+function spawnPromise(...args) {
+    return new Promise(resolve => {
+        const child = __spawn(...args);
+        child.on("close", () => {
+            resolve();
+        });
+        child.on("error", error => {
+            console.log("Encountered an error while spawning:");
+            console.log("Args:", JSON.stringify(args));
+            console.log("Error:", error);
+        });
+    });
+}
+
+if(process.argv.includes("--dev")) {
+    spawnPromise("python", ["main.py"], {
+        stdio: "inherit"
+    });
+    spawnPromise("npm", ["run", "dev"], {
+        stdio: "inherit"
+    });
+} else {
+    if(!existsSync("./.next/BUILD_ID") || process.argv.includes("--build")) {
+        console.log("Compiling...");
+        await spawnPromise("npm", ["run", "build"], {
+            stdio: "inherit"
+        });
+    }
+    
+    spawnPromise("python", ["main.py", "--production"], {
+        stdio: "inherit"
+    });
+    spawnPromise("npm", ["run", "start"], {
+        stdio: "inherit"
+    });
+}

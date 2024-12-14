f7629f810b64b687db88b0eed943a53fe30f5d49
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2024-12-14 13:22:01 +0800

    main.py ready for production

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+from sys import argv
 from multiprocessing import Process, Queue as MultiQueue
 from server.routes import app
 from server.routes import set_command_queue
 from server.routes import set_streaming_queue
 from server.processor import process_commands
 from server.Command import Command
+from waitress import serve
 
 def start_server(
     command_queue: "MultiQueue[Command]",
@@ -11,7 +13,10 @@ def start_server(
 ):
     set_command_queue(command_queue)
     set_streaming_queue(global_streaming_queue)
-    app.run(port=2778, debug=False)
+    if("--production" in argv):
+        serve(app, port=2778)
+    else:
+        app.run(port=2778)
 
 if __name__ == "__main__":
     command_queue = MultiQueue()

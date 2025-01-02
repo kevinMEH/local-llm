c2607a694667cfaa07473087f00141d716229782
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2025-01-02 12:07:02 -0500

    Massive slides styling changes

--- app/welcome/SlideCommons.tsx
+++ app/welcome/SlideCommons.tsx
@@ -1,49 +1,37 @@
-import ArrowLeftIcon from "@/design/icons/ArrowLeftIcon";
 import ArrowRightIcon from "@/design/icons/ArrowRightIcon";
 import { Dispatch, SetStateAction } from "react";
 
 type SlideParameters = {
     active: boolean,
     widthTailwindClass: string,
+    className?: string,
     children: React.ReactNode
 }
 
-export function Slide({ active, widthTailwindClass, children }: SlideParameters) {
-    return <div className="h-screen flex items-center">
-        <div className={`${active ? "opacity-100" : "opacity-50"} transition-opacity ${widthTailwindClass}`}>
+export function Slide({ active, widthTailwindClass, className = "", children }: SlideParameters) {
+    return <div className="h-screen flex items-center px-8 pb-20 pt-16">
+        <div className={`${active ? "opacity-100" : "opacity-50"} transition-opacity ${widthTailwindClass} ${className}`}>
             { children }
         </div>
     </div>
 }
 
-type ButtonParameters = {
+export function NextButton({ setActiveIndex, disabled }: {
     setActiveIndex: Dispatch<SetStateAction<number>>,
     disabled: boolean
-}
-
-export function NextButton({ setActiveIndex, disabled }: ButtonParameters) {
+}) {
     return <button
-        className={`border border-highlight ${disabled ? "text-quiet opacity-50" : "text-sub hover:bg-bg-light"}
-        transition-colors w-52 py-3 rounded-md text-sub flex gap-3 items-center justify-center`}
+        className={`w-80 py-3 rounded-md border border-highlight
+        bg-bg-light ${disabled ? "text-quiet opacity-50" : "text-sub hover:bg-bg-mid"}
+        transition-colors flex gap-2 items-center justify-center`}
         onClick={() => setActiveIndex(old => old + 1)}
         disabled={disabled}
     >
-        <span className="font-mono pl-1">Next</span>
-        <ArrowRightIcon height={17} width={12} />
+        <span>Next</span>
+        <ArrowRightIcon height={20} width={20} />
     </button>
 }
 
-export function PreviousButton({ setActiveIndex, disabled }: ButtonParameters) {
-    return <button
-        className={`border border-highlight ${disabled ? "text-quiet opacity-50" : "text-sub hover:bg-bg-light"}
-        transition-colors w-28 py-2.5 rounded-md text-sub flex gap-3 items-center justify-center`}
-        onClick={() => setActiveIndex(old => old - 1)}
-        disabled={disabled}
-    >
-        <ArrowLeftIcon height={17} width={12} />
-        <span className="font-mono pr-1">Back</span>
-    </button>
-}
 
 export type ConstructedSlideParameters = {
     active: boolean,
--- app/welcome/WelcomeSlide.tsx
+++ app/welcome/WelcomeSlide.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import { ConstructedSlideParameters, NextButton, Slide } from "./SlideCommons";
 
-
 export default function WelcomeSlide({ active, widthTailwindClass, setActiveIndex, setLoadingCount }: ConstructedSlideParameters) {
     const [ hasInternet, setHasInternet ] = useState(true);
 
@@ -14,27 +13,31 @@ export default function WelcomeSlide({ active, widthTailwindClass, setActiveInde
     }, [setLoadingCount]);
 
     return <Slide active={active} widthTailwindClass={widthTailwindClass}>
-        <div className={`space-y-4 text-sub ${active ? "" : "select-none"}`}>
-            <h1 className="text-3xl font-semibold text-main">Welcome to Local LLM!</h1>
-            <p>
-                Welcome to Local LLM! Local LLM is an application that enables
-                you to run Large Language Models (LLMs) and other AI models
-                locally on your computer without requiring access to the internet.
-            </p>
-            { hasInternet
-            ? <p>
-                The next few slides will guide you through the setup process for
-                Local LLM. Note that an internet connection is required to
-                download models for offline use.
-            </p>
-            : <p>
-                Unfortunately, an internet connection is required for initial setup
-                and to download models for offline use. Please connect to the
-                internet before continuing with the setup process.
-            </p> }
-        </div>
-        <div className="flex justify-end">
-            <NextButton setActiveIndex={setActiveIndex} disabled={!hasInternet || !active} />
+        <div className="space-y-8 text-sub text-justify">
+            <h1 className="text-3xl font-semibold text-main text-center">
+                Welcome to Local LLM!
+            </h1>
+            <div className="space-y-4 pb-2">
+                <p>
+                    Welcome to Local LLM! Local LLM enables you to run Large Language
+                    Models and other AI models locally on your computer without
+                    requiring access to the internet.
+                </p>
+                { hasInternet
+                ? <p>
+                    The next few slides will guide you through the setup process for
+                    Local LLM. Note that an internet connection is required to
+                    download models for offline use.
+                </p>
+                : <p>
+                    Unfortunately, an internet connection is required for initial setup
+                    and to download models for offline use. Please connect to the
+                    internet before continuing with the setup process.
+                </p> }
+            </div>
+            <div className="flex justify-center">
+                <NextButton setActiveIndex={setActiveIndex} disabled={!hasInternet} />
+            </div>
         </div>
     </Slide>
 }

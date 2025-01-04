21e66ce2962a9795327119c3c35e146f0f0f031e
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2025-01-04 14:07:04 -0500

    DownloadSlide preliminary

--- app/welcome/DownloadSlide.tsx
+++ app/welcome/DownloadSlide.tsx
@@ -4,8 +4,11 @@ import Image from "next/image";
 
 import RefreshIcon from "@/design/icons/RefreshIcon";
 import { ConstructedSlideParameters, NextButton, Slide } from "./SlideCommons";
-import { getGeneratorBatch, InfiniteAsyncGenerator, listModels, ModelEntry } from "../api/client_models";
+import { bytesToReadable, getGeneratorBatch, InfiniteAsyncGenerator, listModels, ModelEntry, numberToReadable } from "../api/client_models";
 import { getCache, HFCache, HFRepo } from "../api/server_models";
+import HeartIcon from "@/design/icons/HeartIcon";
+import ScrollHint from "./ScrollHint";
+import DownloadIcon from "@/design/icons/DownloadIcon";
 
 export default function DownloadSlide({ active, setActiveIndex, setLoadingCount }: ConstructedSlideParameters) {
     const [ cache, setCache ] = useState<HFCache>();
@@ -30,35 +33,44 @@ export default function DownloadSlide({ active, setActiveIndex, setLoadingCount 
         getCache().then(cache => setCache(cache));
     }
     
-    return <Slide active={active}>
+    return <Slide className="!max-w-none" active={active}>
         <div className="space-y-8 text-sub">
             <h1 className="text-3xl font-semibold text-main text-center">Downloading Models</h1>
-            <div className="space-y-4 pb-2 text-justify">
-                <p>
+            <div className="space-y-4 pb-2 text-justify flex flex-col items-center">
+                <p className="max-w-[52rem]">
                     Browse from a selection of trending models from the search
                     box below, or find your own model on <Link href="https://huggingface.co/models?pipeline_tag=text-generation&sort=trending" target="_blank">
                         Huggingface
                     </Link> and input the model name (Example: meta-llama/Llama-3.2-1B-Instruct)
                     into the search bar.
                 </p>
-                <div className="space-y-4 mx-auto max-w-[30rem] max-h-96 flex flex-col">
-                    <div className="flex justify-between items-center">
-                        <h2 className="text-xl font-semibold text-main">Downloaded Models:</h2>
-                        <button
-                            className="px-5 py-2.5
-                            rounded-md border border-highlight bg-transparent hover:bg-bg-mid
-                            transition-colors flex gap-2 items-center justify-center"
-                            onClick={refreshCache}
-                        >
-                            <span className="font-mono text-sm">Refresh</span>
-                            <RefreshIcon size={14} />
-                        </button>
+                <div className="*:h-[22rem] grid grid-cols-2 gap-8 min-w-0">
+                    <div
+                        className="border border-highlight rounded-xl
+                        max-w-[30rem] h-full px-4 py-3 flex flex-col min-w-0"
+                    >
+                        <div className="flex justify-between items-center pb-3 px-3">
+                            <h2 className="text-xl font-semibold text-main pt-1">Downloaded Models:</h2>
+                            <button
+                                className="px-3 py-2
+                                rounded-md border border-highlight bg-transparet hover:bg-bg-mid
+                                transition-colors flex gap-2 items-center justify-center"
+                                onClick={refreshCache}
+                            >
+                                <span className="font-mono text-xs">Refresh</span>
+                                <RefreshIcon size={12} />
+                            </button>
+                        </div>
+                        <ScrollHint className="overflow-y-scroll hide-scrollbar">
+                            { cache?.repos.map(
+                                (repo, i) => <DownloadedModelElement key={i} repo={repo} />
+                            ) }
+                        </ScrollHint>
                     </div>
-                    <div className="overflow-y-scroll invert-scrollbar-color border border-highlight rounded-xl pl-[15px] py-3 pr-0">
-                        { cache?.repos.map(
-                            (repo, i) => <ExistingModelElement key={i} repo={repo} />
-                        ) }
-                    </div>
+                    <ModelPageDisplay cache={cache}
+                        className="border border-highlight rounded-xl
+                        max-w-[30rem] h-full px-4 py-3 flex flex-col min-w-0"
+                    />
                 </div>
             </div>
             <div className="flex justify-center">
@@ -68,31 +80,13 @@ export default function DownloadSlide({ active, setActiveIndex, setLoadingCount 
     </Slide>
 }
 
-/**
- * You may wonder why 1000 is used instead of 1024:
- * We always consider the worst case scenario for the user, thus we try to make
- * the storage size as large as possible.
- */
- function bytesToReadable(bytes: number): string {
-    const units = [ "B", "KB", "MB", "GB" ];
-    let unitsIndex = 0;
-    while(bytes > 1000 && unitsIndex < units.length - 1) {
-        bytes = bytes / 1000;
-        unitsIndex += 1;
-    }
-    return bytes.toFixed(2) + " " + units[unitsIndex] + "s";
-}
-
-function ExistingModelElement({ repo }: { repo: HFRepo }) {
-    const {
-        repo_id, size_on_disk, nb_files, last_accessed
-    } = repo;
+function DownloadedModelElement({ repo }: { repo: HFRepo }) {
+    const { repo_id, size_on_disk, nb_files, last_accessed } = repo;
     const lastAccessedDate = new Date(last_accessed * 1000);
     const readableSizeOnDisk = bytesToReadable(size_on_disk);
     
     const repoOwner = repo_id.substring(0, repo_id.indexOf("/"));
-    
-    const [ avatarUrl, setAvatarUrl ] = useState<string>("https://cdn-avatars.huggingface.co/v1/production/uploads/64a32c8cd9dd1da3508366b0/HyiBEJCSZa-YXSGRH1nzi.png");
+    const [ avatarUrl, setAvatarUrl ] = useState<string | undefined>(undefined);
     useEffect(() => {
         (async () => {
             const response = await fetch(`https://huggingface.co/api/organizations/${repoOwner}/avatar`);
@@ -109,19 +103,24 @@ function ExistingModelElement({ repo }: { repo: HFRepo }) {
         })();
     }, [repoOwner]);
     
-    return <div className="border border-highlight first:mt-0 -mt-[1px] first:rounded-t-md last:rounded-b-md px-5 py-3 flex gap-5">
-        <div className="flex items-center">
-            <Image src={avatarUrl} alt={`${repoOwner}'s avatar`} width={40} height={40} />
+    return <div className="border border-highlight first:mt-0 -mt-[1px]
+    first:rounded-t-md last:rounded-b-md px-5 py-3 flex gap-5 min-w-0">
+        <div className="flex items-center w-10">
+            {
+                avatarUrl
+                ?  <Image className="rounded-md" src={avatarUrl} alt={`${repoOwner}'s avatar`} width={40} height={40} />
+                : <div className="rounded-full border box-content border-highlight w-10 h-10" />
+            }
         </div>
-        <div>
-            <h3 className="font-mono text-sm">{repo_id}</h3>
-            <p className="text-xs text-quiet">Last accessed: {lastAccessedDate.toLocaleString()}</p>
-            <p className="text-xs text-quiet">{nb_files} file(s) occupying {readableSizeOnDisk}</p>
+        <div className="text-left min-w-0 *:overflow-hidden *:text-ellipsis whitespace-nowrap">
+            <h3 className="font-mono text-sm min-w-0">{repo_id}</h3>
+            <p className="text-xs text-quiet">{`Last accessed: ${lastAccessedDate.toLocaleString()}`}</p>
+            <p className="text-xs text-quiet">{nb_files} files occupying {readableSizeOnDisk}</p>
         </div>
     </div>
 }
 
-function ModelPageDisplay({ cache }: { cache: HFCache }) {
+function ModelPageDisplay({ cache, className }: { cache: HFCache | undefined, className: string }) {
     const [ generator, setGenerator ] = useState<InfiniteAsyncGenerator<ModelEntry, undefined>>(listModels());
     const [ models, setModels ] = useState<ModelEntry[]>([]);
     const [ page, setPage ] = useState(0);
@@ -160,14 +159,14 @@ function ModelPageDisplay({ cache }: { cache: HFCache }) {
                 if(nextBatch.length === 0) {
                     setPageLimit(page);
                 } else {
-                    setModels(models.concat(nextBatch));
+                    setModels(models => models.concat(nextBatch));
                     if(nextBatch.length < itemsPerPage) {
                         setPageLimit(page + 1);
                     }
                 }
             })();
         }
-    }, [ generator, page, models ]);
+    }, [ generator, page ]);
     
     function setSearch(event: ChangeEvent<HTMLInputElement>) {
         clearTimeout(searchTimeout);
@@ -180,14 +179,15 @@ function ModelPageDisplay({ cache }: { cache: HFCache }) {
     
     const currentPageModels = models.slice(page * itemsPerPage, (page + 1) * itemsPerPage);
 
-    return <div className="rounded-lg border border-highlignt p-5">
-        <input
-            className="font-mono bg-transparent rounded-md border border-highlight px-5 py-3 outline-none w-full placeholder:text-quiet"
+    return <div className={className}>
+        {/* <input
+            className="font-mono bg-transparent rounded-md border text-sm border-highlight px-5 py-3 outline-none placeholder:text-quiet"
             value={searchText} onChange={setSearch}
             placeholder="Search for models..."
-        />
-        <div>
-        </div>
+        /> */}
+        <ScrollHint className="overflow-y-scroll hide-scrollbar">
+            { currentPageModels.map((model, i) => <ListedModelElement key={i} model={model} /> )}
+        </ScrollHint>
     </div>
 }
 
@@ -198,18 +198,49 @@ function ModelPageDisplay({ cache }: { cache: HFCache }) {
     downloads: number;
     tags: string[];
     createdAt: string;
-    
-    repo_id: string,
-    repo_type: string,
-    repo_path: string,
-    size_on_disk: number,
-    nb_files: number,
-    revisions: HFRevision[],
-    last_accessed: number,
-    last_modified: number
 */
-function ModelElement({ model }: { model: ModelEntry | HFRepo }) {
-    return <div>
-        
+function ListedModelElement({ model }: { model: ModelEntry }) {
+    const { id, likes, downloads } = model;
+    
+    const repoOwner = id.substring(0, id.indexOf("/"));
+    const [ avatarUrl, setAvatarUrl ] = useState<string | undefined>(undefined);
+    useEffect(() => {
+        (async () => {
+            const response = await fetch(`https://huggingface.co/api/organizations/${repoOwner}/avatar`);
+            const data = await response.json();
+            if(data.avatarUrl) {
+                setAvatarUrl(data.avatarUrl as string);
+            } else {
+                const response = await fetch(`https://huggingface.co/api/users/${repoOwner}/avatar`);
+                const data = await response.json();
+                if(data.avatarUrl) {
+                    setAvatarUrl(data.avatarUrl as string);
+                }
+            }
+        })();
+    }, [repoOwner]);
+    
+    return <div className="border border-highlight first:mt-0 -mt-[1px]
+    first:rounded-t-md last:rounded-b-md px-5 py-3 flex gap-5 min-w-0">
+        <div className="flex items-center w-10">
+            {
+                avatarUrl
+                ?  <Image className="rounded-md" src={avatarUrl} alt={`${repoOwner}'s avatar`} width={40} height={40} />
+                : <div className="rounded-full border box-content border-highlight w-10 h-10" />
+            }
+        </div>
+        <div className="text-left min-w-0 space-y-1 *:overflow-hidden *:text-ellipsis whitespace-nowrap">
+            <h3 className="font-mono text-sm min-w-0">{id}</h3>
+            <p className="text-xs flex gap-3 text-quiet">
+                <span className="flex gap-1 text-xs items-center">
+                    <HeartIcon size={14} />
+                    <span>{ numberToReadable(likes) }</span>
+                </span>
+                <span className="flex gap-1 text-xs items-center">
+                    <DownloadIcon size={14} />
+                    <span>{ numberToReadable(downloads) }</span>
+                </span>
+            </p>
+        </div>
     </div>
 }

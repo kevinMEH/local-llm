58c7ea766eebc5d52ccc66a9383ee465efb2b32a
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2025-01-04 14:06:50 -0500

    ScrollHint.tsx

---
+++ app/welcome/ScrollHint.tsx
@@ -0,0 +1,67 @@
+import { useEffect, useRef, useState } from "react";
+
+export default function ScrollHint({ className, children }: { className: string, children: React.ReactNode }) {
+    const [ topShadow, setTopShadow ] = useState(false);
+    const [ bottomShadow, setBottomShadow ] = useState(false);
+    
+    const elementRef = useRef<HTMLDivElement>(null);
+    
+    function toppedOut() {
+        const element = elementRef.current;
+        if(element) {
+            return element.scrollTop < 1;
+        }
+        return true;
+    }
+    
+    function bottomedOut() {
+        const element = elementRef.current;
+        if(element) {
+            return Math.abs(element.scrollHeight - element.clientHeight - element.scrollTop) <= 1;
+        }
+        return false;
+    }
+    
+    function overflowing() {
+        const element = elementRef.current;
+        if(element) {
+            return element.scrollHeight > element.clientHeight;
+        }
+        return false;
+    }
+    
+    function scrollHandler() {
+        if(overflowing()) {
+            setBottomShadow(!bottomedOut());
+            setTopShadow(!toppedOut());
+        } else {
+            setBottomShadow(false);
+            setTopShadow(false);
+        }
+    }
+    
+    useEffect(() => {
+        if(overflowing()) {
+            setBottomShadow(!bottomedOut());
+            setTopShadow(!toppedOut());
+        } else {
+            setBottomShadow(false);
+            setTopShadow(false);
+        }
+    }, [children])
+    
+    const shadowClass = bottomShadow
+    ? (
+        topShadow
+        ? "[mask-image:linear-gradient(transparent,#000_36px,#000_calc(100%_-_36px),transparent)]" // Both
+        : "[mask-image:linear-gradient(180deg,#000_calc(100%_-_36px),transparent)]" // Bottom only
+    ) : (
+        topShadow
+        ? "[mask-image:linear-gradient(0deg,#000_calc(100%_-_36px),transparent)]" // Top only
+        : ""
+    );
+
+    return <div ref={elementRef} className={`${shadowClass} ${className}`} onScroll={scrollHandler}>
+        { children }
+    </div>
+}
